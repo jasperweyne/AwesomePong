08d4bedbfa1df93a43273bf926a5b7fbd55cc8c0
Jasper Weyne <jasperweyne@gmail.com>
2016-10-02 22:46:25 +0200

    Moaar challenge

--- Pong/Ball.cs
+++ Pong/Ball.cs
@@ -9,6 +9,7 @@ namespace Pong
     {
         private static Random r = new Random();
         private Player last;
+        private static float speed = 4;
 
         public Ball(Texture2D tex): base(tex)
         {
@@ -18,7 +19,6 @@ namespace Pong
                 effect = SpriteEffects.FlipHorizontally;
             }
             dir -= Math.PI / 4;
-            const double speed = 4;
             this.movement = new Vector2((float)(speed * Math.Cos(dir)), (float)(speed * Math.Sin(dir)));
             this.location = MainProcess.GState.Field.Center.ToVector2();
             this.Bounds = new Rectangle(location.ToPoint(), new Point(84, 64));
@@ -32,7 +32,6 @@ namespace Pong
                 effect = SpriteEffects.FlipHorizontally;
             }
             dir -= Math.PI / 4;
-            const double speed = 4;
             this.movement = new Vector2((float)(speed * Math.Cos(dir)), (float)(speed * Math.Sin(dir)));
             this.location = MainProcess.GState.Field.Center.ToVector2();
         }
@@ -56,32 +55,35 @@ namespace Pong
         {
             Vector2 oldLoc = this.location;
 
-            if (this.location.Y <= MainProcess.GState.Field.Top) {
-                Player hit = getPlayer(Player.ScreenLocation.Top);
-                if (hit == null)
-                    this.movement.Y = Math.Abs(this.movement.Y);
-                else if (this.location.Y + this.Bounds.Height <= MainProcess.GState.Field.Top)
-                    MainProcess.GState.Hit(this, hit, last);
-            } else if (this.location.Y + this.Bounds.Height >= MainProcess.GState.Field.Bottom) {
-                Player hit = getPlayer(Player.ScreenLocation.Bottom);
-                if (hit == null)
-                    this.movement.Y = -Math.Abs(this.movement.Y);
-                else if (this.location.Y >= MainProcess.GState.Field.Bottom)
-                    MainProcess.GState.Hit(this, hit, last);
-            }
-            if (this.location.X <= MainProcess.GState.Field.Left) {
-                Player hit = getPlayer(Player.ScreenLocation.Left);
-                if (hit == null)
-                    this.movement.X = Math.Abs(this.movement.X);
-                else if (this.location.X + this.Bounds.Width <= MainProcess.GState.Field.Left)
-                    MainProcess.GState.Hit(this, hit, last);
-            } else if (this.location.X + this.Bounds.Width >= MainProcess.GState.Field.Right) {
-                Player hit = getPlayer(Player.ScreenLocation.Right);
-                if (hit == null)
-                    this.movement.X = -Math.Abs(this.movement.X);
-                else if (this.location.X >= MainProcess.GState.Field.Right)
-                    MainProcess.GState.Hit(this, hit, last);
-            }
+            if (MainProcess.GState != null)
+                if (this.location.Y <= MainProcess.GState.Field.Top) {
+                    Player hit = getPlayer(Player.ScreenLocation.Top);
+                    if (hit == null)
+                        this.movement.Y = Math.Abs(this.movement.Y);
+                    else if (this.location.Y + this.Bounds.Height <= MainProcess.GState.Field.Top)
+                        MainProcess.GState.Hit(this, hit, last);
+                } else if (this.location.Y + this.Bounds.Height >= MainProcess.GState.Field.Bottom) {
+                    Player hit = getPlayer(Player.ScreenLocation.Bottom);
+                    if (hit == null)
+                        this.movement.Y = -Math.Abs(this.movement.Y);
+                    else if (this.location.Y >= MainProcess.GState.Field.Bottom)
+                        MainProcess.GState.Hit(this, hit, last);
+                }
+
+            if (MainProcess.GState != null)
+                if (this.location.X <= MainProcess.GState.Field.Left) {
+                    Player hit = getPlayer(Player.ScreenLocation.Left);
+                    if (hit == null)
+                        this.movement.X = Math.Abs(this.movement.X);
+                    else if (this.location.X + this.Bounds.Width <= MainProcess.GState.Field.Left)
+                        MainProcess.GState.Hit(this, hit, last);
+                } else if (this.location.X + this.Bounds.Width >= MainProcess.GState.Field.Right) {
+                    Player hit = getPlayer(Player.ScreenLocation.Right);
+                    if (hit == null)
+                        this.movement.X = -Math.Abs(this.movement.X);
+                    else if (this.location.X >= MainProcess.GState.Field.Right)
+                        MainProcess.GState.Hit(this, hit, last);
+                }
             UpdateLoc();
             
             if (MainProcess.GState != null)
@@ -91,7 +93,13 @@ namespace Pong
                             Rectangle dBoundsX = new Rectangle(new Point((int)(oldLoc.X + this.movement.X), (int)oldLoc.Y), this.Bounds.Size);
                             if (dBoundsX.Intersects(elem.GetBounds())) {
                                 this.location.Y = oldLoc.Y;
-                                this.movement.X *= -1;
+                                //this.movement.X *= -1;
+                                double dy = (elem.GetBounds().Center.Y - this.Bounds.Center.Y) / (double)(elem.GetBounds().Height + this.Bounds.Height) * Math.PI / 3 * 2;
+                                if (this.movement.X > 0)
+                                    dy += Math.PI;
+                                Vector2 newMove = new Vector2((float)Math.Cos(dy), (float)Math.Sin(dy));
+                                this.movement = newMove * speed;
+                                Console.WriteLine(this.movement);
                                 if (elem.GetBounds().Intersects(new Rectangle((this.location + this.movement).ToPoint(), this.Bounds.Size))) {
                                     if (elem.Movement.X > 0)
                                         this.location.X = elem.GetBounds().Right - this.movement.X;
@@ -102,7 +110,14 @@ namespace Pong
                             Rectangle dBoundsY = new Rectangle(new Point((int)oldLoc.X, (int)(oldLoc.Y + this.movement.Y)), this.Bounds.Size);
                             if (dBoundsY.Intersects(elem.GetBounds())) {
                                 this.location.X = oldLoc.X;
-                                this.movement.Y *= -1;
+                                //this.movement.Y *= -1;
+                                double dx = (elem.GetBounds().Center.X - this.Bounds.Center.X) / (double)(elem.GetBounds().Width + this.Bounds.Width) * Math.PI / 3 * 2;
+                                if (this.movement.Y > 0)
+                                    dx += Math.PI / 2 * 3;
+                                else
+                                    dx += Math.PI / 2;
+                                Vector2 newMove = new Vector2((float)Math.Cos(dx), (float)Math.Sin(dx));
+                                this.movement = newMove * speed;
                                 if (elem.GetBounds().Intersects(new Rectangle((this.location + this.movement).ToPoint(), this.Bounds.Size))) {
                                     if (elem.Movement.Y > 0)
                                         this.location.Y = elem.GetBounds().Bottom - this.movement.Y;
@@ -112,6 +127,7 @@ namespace Pong
                             }
                             UpdateLoc();
                             this.last = (Player)elem;
+                            speed += 0.1f;
                         }
                     }
                 }
