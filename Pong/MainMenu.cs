90d9ec834c8d2435cf266950b428999bf21119d6
Jasper Weyne <jasperweyne@gmail.com>
2016-10-02 21:44:12 +0200

    Space for player setup

--- Pong/MainMenu.cs
+++ Pong/MainMenu.cs
@@ -1,16 +1,74 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pong
 {
     class MainMenu: State
     {
+        List<PlayerDef> players = new List<PlayerDef>() {
+            new PlayerDef(PlayerType.Playable, Color.Yellow, Player.ScreenLocation.Left, KeySet.set1),
+            new PlayerDef(PlayerType.AI, Color.Blue, Player.ScreenLocation.Right, KeySet.set2),
+        };
+
+        struct KeySet
+        {
+            public Keys up;
+            public Keys down;
+            public Keys left;
+            public Keys right;
+            public static KeySet set1 = new KeySet(Keys.W, Keys.S, Keys.A, Keys.D);
+            public static KeySet set2 = new KeySet(Keys.Up, Keys.Down, Keys.Left, Keys.Right);
+            public static KeySet set3 = new KeySet(Keys.G, Keys.B, Keys.V, Keys.N);
+            public static KeySet set4 = new KeySet(Keys.D9, Keys.O, Keys.I, Keys.P);
+
+            public KeySet(Keys up, Keys down, Keys left, Keys right)
+            {
+                this.up = up;
+                this.down = down;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        struct PlayerDef
+        {
+            public PlayerType type;
+            public Color color;
+            public Player.ScreenLocation loc;
+            public KeySet keys;
+
+
+            public PlayerDef(PlayerType type, Color col, Player.ScreenLocation loc, KeySet keys)
+            {
+                this.type = type;
+                this.color = col;
+                this.loc = loc;
+                this.keys = keys;
+            } 
+        }
+        private enum PlayerType
+        {
+            None,
+            Playable,
+            AI
+        }
+
         public override void Update()
         {
+
             if (Keyboard.GetState().IsKeyDown(Keys.Enter)) {
                 MainProcess.State = new GameState();
-                MainProcess.GState.Elems.Add(new PlayablePlayer(MainProcess.PlayerTex, Keys.S, Keys.W, Player.ScreenLocation.Left, Color.Yellow));
-                MainProcess.GState.Elems.Add(new PlayablePlayer(MainProcess.PlayerTex, Keys.Down, Keys.Up, Player.ScreenLocation.Right, Color.Blue));
+                foreach (PlayerDef def in players.Where(player => player.type != PlayerType.None)) {
+                    if (def.type == PlayerType.Playable)
+                        if (def.loc == Player.ScreenLocation.Left || def.loc == Player.ScreenLocation.Right)
+                            MainProcess.GState.Elems.Add(new PlayablePlayer(MainProcess.PlayerTex, def.keys.down, def.keys.up, def.loc, def.color));
+                        else
+                            MainProcess.GState.Elems.Add(new PlayablePlayer(MainProcess.PlayerTex, def.keys.right, def.keys.left, def.loc, def.color));
+                    else
+                        MainProcess.GState.Elems.Add(new AIPlayer(MainProcess.PlayerTex, def.loc, def.color));
+                }
                 MainProcess.GState.Elems.Add(new Ball(MainProcess.BallTex));
             }
         }
